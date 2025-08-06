223f91e437a287f5c185d242e7018fb01b24e10e
Fern-Aerell <fernaerell2020@gmail.com>
2025-08-06 22:32:06 +0700

    create: Enum auto editor export

---
+++ aerell_auto_editor_gui/ae_export_enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class AEExportEnum(Enum):
+    PREMIERE = 'premiere',
+    RESOLVE = 'resolve',
+    FINAL_CUT_PRO = 'final-cut-pro',
+    SHOTCUT = 'shotcut',
+    CLIP_SEQUENCE = 'clip-sequence',
